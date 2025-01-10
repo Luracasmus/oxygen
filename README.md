90d9d1b0d48fbed97f7d77d95003a09fb396e845
Lura <77991691+Luracasmus@users.noreply.github.com>
2025-01-10 15:29:12 +0100

    Major improvements

--- README.md
+++ README.md
@@ -16,6 +16,6 @@ To install a crate, run Oxygen with the path to an Oxygen Manifest (in RON forma
 ```
 `> oxygen "path/to/circles.o2"`
 
-The crate will be installed in a directory next to Oxygen itself, and can be started by simply opening the manifest again (you can even associate Oxygen with the `o2` file extension to make the manifest work almost like a shortcut!). Every time the manifest is ran, the crate, along with Rustup and the Rust toolchain are automatically updated. All compiled dependencies are cached and shared between crates
+The crate will be installed in a directory next to Oxygen itself, and can be started by simply opening the manifest again (you can even associate Oxygen with the `o2` file extension to make the manifest work almost like a shortcut!). Every time the manifest is ran, the crate, along with Rustup and the Rust toolchain are automatically updated. If the crate is already installed, updates run after it exits to minimize launch time. All compiled dependencies are cached and shared between crates
 
 Oxygen requires Rustup and Cargo to be installed
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::{env, ffi::OsStr, fs::File, path::{Path, PathBuf}, process::Command, thread::sleep, time::Duration};
+use std::{env::{self, consts::EXE_SUFFIX}, ffi::OsStr, fs::File, io, path::{Path, PathBuf}, process::Command};
 
 use serde::Deserialize;
-
-const EXE_EXT: &str = if cfg!(target_os = "windows") { "exe" } else { "" };
+use windows::Win32::System::Console;
 
 #[derive(Deserialize)]
 struct Manifest {
@@ -66,28 +65,22 @@ fn get_manifest() -> Option<Manifest> {
 	}
 }
 
-fn main() {
-	let mut problem = false;
-	
-	let update = match Command::new("rustup").arg("update").spawn() {
-		Ok(update) => Some(update),
-		Err(err) => {
-			problem = true;
-			println!("Failed to spawn 'rustup update'\n{err}\nProceeding anyway...");
-			None
-		}
-	};
+fn update_rustup(problem: &mut bool) {
+	if let Err(err) = Command::new("rustup").arg("update").status() {
+		*problem = true;
+		println!("Rustup Update failed!\n{err}");
+	}
+}
 
-	if let Some(manifest) = get_manifest() {
-		let current_exe = env::current_exe().unwrap().into_boxed_path();
-		let install_root = current_exe.parent().unwrap().to_str().unwrap();
+fn install(manifest: &Manifest, root: &str, problem: &mut bool) {
+	update_rustup(problem);
 
-		let mut install = Command::new("cargo");
+	let mut install = Command::new("cargo");
 		install.args([
 			"install",
-			"--root", install_root,
-			"--target-dir", &(install_root.to_string() + "/cache"),
-			"--config", "rustflags = [\"-C\", \"target-cpu=native\", \"-C\", \"link-arg=-fuse-ld=lld\"]",
+			"--root", root,
+			"--target-dir", &(root.to_string() + "/cache"),
+			"--config", "rustflags = [\"-C\", \"target-cpu=native\"]",
 
 			"--config", "profile.release.lto = \"thin\"",
 			"--config", "profile.release.opt-level = 3",
@@ -105,70 +98,93 @@ fn main() {
 			"--config", "profile.release.package.\"*\".codegen-units = 1"
 		]);
 
-		match manifest.path {
-			Crate::CratesIO(name) => { install.arg(name.as_ref()); },
-			Crate::Git{url, path} => {
-				install.args(["--git", &url]);
+	match &manifest.path {
+		Crate::CratesIO(name) => { install.arg(name.as_ref()); },
+		Crate::Git{url, path} => {
+			install.args(["--git", url]);
 
-				if let Some(path) = path { install.arg(path.as_ref()); }
-			},
-			Crate::Local(path) => { install.args(["--path", path.to_str().unwrap()]); },
-		};
+			if let Some(path) = path { install.arg(path.as_ref()); }
+		},
+		Crate::Local(path) => { install.args(["--path", path.to_str().unwrap()]); },
+	};
 
-		match manifest.features {
-			Features::Default => (),
-			Features::None => { install.arg("--no-default-features"); },
-			Features::All => { install.arg("--all-features"); },
-			Features::Add(features) => { install.args(features.iter().map(AsRef::as_ref)); },
-			Features::Replace(features) => { install.arg("--no-default-features").args(features.iter().map(AsRef::as_ref)); }
-		}
+	match &manifest.features {
+		Features::Default => (),
+		Features::None => { install.arg("--no-default-features"); },
+		Features::All => { install.arg("--all-features"); },
+		Features::Add(features) => { install.args(features.iter().map(AsRef::as_ref)); },
+		Features::Replace(features) => { install.arg("--no-default-features").args(features.iter().map(AsRef::as_ref)); }
+	}
 
-		if let Some(mut update) = update {
-			if let Err(err) = update.wait() {
-				problem = true;
-				println!("Rustup Update failed!\n{err}\nProceeding anyway...");
-			}
-		}
+	if let Err(err) = install.status() {
+		*problem = true;
+		println!("Cargo failed to install the application!\n{err}");
+	}
+}
 
-		let install = install.spawn();
+fn run(path: &Path, always_show_console: bool, problem: &mut bool) {
+	let mut app = Command::new(path);
 
-		let mut app = Command::new(format!("{install_root}/bin/{}.{EXE_EXT}", manifest.name));
+	// Remove Terminal window when spawning
+	#[cfg(target_os = "windows")]
+	std::os::windows::process::CommandExt::creation_flags(&mut app, 0x0800_0000);
 
-		// Remove Terminal window when spawning app
-		#[cfg(target_os = "windows")]
-		std::os::windows::process::CommandExt::creation_flags(&mut app, 0x0800_0000);
-
-		match install {
-			Ok(mut install) => match install.wait() {
-				Ok(_) => (),
-				Err(err) => {
-					problem = true;
-					println!("Cargo Install failed!\n{err}\nAttempting to spawn app anyway...");
-				}
-			},
-			Err(err) => {
-				problem = true;
-				println!("Failed to spawn 'cargo install'!\n{err}\nAttempting to spawn app anyway...");
-			}
-		}
-
-		if let Err(err) = app.spawn() {
-			problem = true;
+	let app = app.spawn();
+	match app {
+		Err(err) => {
+			*problem = true;
 			println!("Failed to spawn app!\n{err}");
 		}
-	} else {
-		problem = true;
-		println!("Aborting app installation/startup\nWaiting for Rustup Update to finish...");
+		Ok(mut app) => {
+			#[cfg(target_os = "windows")]
+			unsafe { Console::FreeConsole() }.unwrap();
 
-		if let Some(mut update) = update {
-			if let Err(err) = update.wait() {
-				println!("Rustup Update failed!\n{err}");
+			let app_result = app.wait();
+
+			#[cfg(target_os = "windows")]
+			if always_show_console {
+				unsafe { Console::AllocConsole() }.unwrap();
+			}
+
+			if let Err(err) = app_result {
+				#[cfg(target_os = "windows")]
+				if !always_show_console {
+					unsafe { Console::AllocConsole() }.unwrap();
+				}
+
+				*problem = true;
+				println!("Failed to spawn app!\n{err}");
 			}
 		}
 	}
+}
+
+fn main() {
+	let mut problem = false;
+
+	if let Some(manifest) = get_manifest() {
+		let current_exe = env::current_exe().unwrap().into_boxed_path();
+		let install_root = current_exe.parent().unwrap().to_str().unwrap();
+
+		let app_path = PathBuf::from(&format!("{install_root}/bin/{}{EXE_SUFFIX}", manifest.name)).into_boxed_path();
+
+		if app_path.exists() {
+			run(&app_path, true, &mut problem);
+			install(&manifest, install_root, &mut problem);
+		} else {
+			install(&manifest, install_root, &mut problem);
+			run(&app_path, false, &mut problem);
+		}
+	} else {
+		problem = true;
+		println!("Failed to parse manifest. App update/startup cancelled");
+
+		update_rustup(&mut problem);
+	}
 
 	if problem {
-		println!("Exiting automatically in 10 seconds...");
-		sleep(Duration::from_secs(10));
+		println!("Press enter to exit");
+		let mut temp = String::new();
+		io::stdin().read_line(&mut temp).unwrap();
 	}
 }
